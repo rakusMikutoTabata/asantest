809bd9739caf30fbf25364a02554872e1fa1cacf
rakusMikutoTabata <mikuto.tabata@rakus.co.jp>
2020-07-09 11:08:32 +0900

    first commit

---
+++ src/main/java/com/example/AsantestApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsantestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsantestApplication.class, args);
+	}
+
+}
